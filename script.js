f67053297eeea1efd0cea560f9e3ce312e058655
BrianLevin <bml201095@gmail.com>
2022-04-01 21:25:29 -0400

    made file corrections

--- script.js
+++ script.js
@@ -6,45 +6,6 @@ document.querySelectorAll('.watch-control, .controls a, .iphone-btn').forEach(co
 })
 // End of Common JS
 
-// Slideshow
-const slideshowDivs = () => {
-    for(let i = 1; i <= 5; i++) {
-        const div = document.createElement('div')
-
-        div.style.backgroundImage = `url(images/slideshow/section-1-bg-${i}.jpg)`
-
-        i === 1 && div.classList.add('change')
-
-        document.querySelector('.slideshow').appendChild(div)
-    }
-}
-
-slideshowDivs()
-
-const divs = document.querySelectorAll('.slideshow div')
-
-let a = 1
-
-const slideshow = () => {
-    setInterval(() => {
-        a++
-
-        const div = document.querySelector('.slideshow .change')
-
-        div.classList.remove('change')
-
-        if(a < divs.length) {
-            div.nextElementSibling.classList.add('change')
-        } else {
-            divs[0].classList.add('change')
-            a = 1
-        }
-    }, 20000)
-}
-
-slideshow()
-// End of Slideshow
-
 // Cube 
 let x = 0
 let y = 20
@@ -102,6 +63,44 @@ document.querySelector('.controls').addEventListener('mouseout', () => {
 
 // End of Cube 
 
+// Slideshow
+const slideshowDivs = () => {
+    for(let i = 1; i <= 5; i++) {
+        const div = document.createElement('div')
+
+        div.style.backgroundImage = `url(images/slideshow/section-1-bg-${i}.jpg)`
+
+        i === 1 && div.classList.add('change')
+
+        document.querySelector('.slideshow').appendChild(div)
+    }
+}
+
+slideshowDivs()
+
+const divs = document.querySelectorAll('.slideshow div')
+
+let a = 1
+
+const slideshow = () => {
+    setInterval(() => {
+        a++
+
+        const div = document.querySelector('.slideshow .change')
+
+        div.classList.remove('change')
+
+        if(a < divs.length) {
+            div.nextElementSibling.classList.add('change')
+        } else {
+            divs[0].classList.add('change')
+            a = 1
+        }
+    }, 20000)
+}
+
+slideshow()
+// End of Slideshow
 
 // Section 3
 const section3Content = document.querySelector('.section-3-content')
@@ -112,7 +111,6 @@ window.addEventListener('scroll', () => {
     }
 })
 // End of Section 3
-
 
 // Section 4
 const watchBands = document.querySelector('.watch-bands')
